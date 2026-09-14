f13ac0b80f89b0bb1b23fbf7bdd09550298249fa
ag12345upta-sketch <ag12345upta@gmail.com>
2026-09-14 12:49:50 +0530

    fix: correct failing test

--- Backend/test/basic.test.js
+++ Backend/test/basic.test.js
@@ -2,7 +2,7 @@ const test = require('node:test');
 const assert = require('node:assert');
 
 test('basic test', () => {
-  assert.strictEqual(1 + 1, 4);
+  assert.strictEqual(1 + 1, 2);
 });const test = require('node:test');
 const assert = require('node:assert');
 
