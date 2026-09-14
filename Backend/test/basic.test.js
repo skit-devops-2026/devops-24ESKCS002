488fbbbb533449a3d46a4b3cd958592a5d283c87
ag12345upta-sketch <ag12345upta@gmail.com>
2026-09-14 13:04:53 +0530

    fix: clean automated test

--- Backend/test/basic.test.js
+++ Backend/test/basic.test.js
@@ -3,9 +3,4 @@ const assert = require('node:assert');
 
 test('basic test', () => {
   assert.strictEqual(1 + 1, 2);
-});const test = require('node:test');
-const assert = require('node:assert');
-
-test('basic test', () => {
-  assert.strictEqual(2 + 2, 4);
 });
